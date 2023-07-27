a536258ecc0bbb7562c06650f1d440cc2ca01fa1
Ali Mahdiyar <alimahdiyar77@gmail.com>
2023-07-27 14:09:32 +0330

    refactor: useContractAddress

--- src/hooks/useContractAddress.ts
+++ src/hooks/useContractAddress.ts
@@ -1,8 +1,9 @@
+import { chains } from 'constants/chains';
 import { useMemo } from 'react';
 import { AddressMap } from 'types';
 import { useNetwork } from 'wagmi';
 
 export function useContractAddress(addressMap: AddressMap) {
-  const { chain, chains } = useNetwork();
-  return useMemo(() => addressMap[chain?.id ?? chains[0].id], [addressMap, chain?.id, chains]);
+  const { chain } = useNetwork();
+  return useMemo(() => addressMap[chain?.id ?? chains[0].id], [addressMap, chain?.id]);
 }
