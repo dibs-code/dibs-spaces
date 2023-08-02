dcc1194d74b7672c392067d3ff9dac74f21e6b13
Amirerfan Siamaki <amirerfan.siamaki@gmail.com>
2023-08-02 20:40:59 +0200

    Epoch Switch

--- src/components/pairIsolated/LeaderBoardEpochButtons.tsx
+++ src/components/pairIsolated/LeaderBoardEpochButtons.tsx
@@ -9,29 +9,54 @@ export function LeaderBoardEpochButtons({
   setEpochToShowWinners: Function;
   activeDay: bigint | undefined;
 }) {
+  const [isCurrentEpoch, setIsCurrentEpoch] = React.useState(true);
+
   const setPreviousEpochToShowWinners = useCallback(() => {
     if (activeDay) {
       setEpochToShowWinners(activeDay - BigInt(1));
     }
   }, [activeDay, setEpochToShowWinners]);
   return (
-    <div className={'flex flex-wrap'}>
-      <button
-        className={`${
-          activeDay && epochToShowWinners === activeDay - BigInt(1) ? 'btn-primary' : 'btn-primary-inverted'
-        } btn-large font-medium mt-4 w-full xl:w-auto px-8`}
-        onClick={setPreviousEpochToShowWinners}
-      >
-        Previous Epoch
-      </button>
-      <button
-        className={`${
-          epochToShowWinners === BigInt(10) ? 'btn-primary' : 'btn-primary-inverted'
-        } btn-large font-medium mt-4 w-full xl:w-auto px-8 border-2 mx-2`}
-        onClick={() => setEpochToShowWinners(BigInt(10))}
-      >
-        Day 10
-      </button>
+    <div className="p-1.5 w-[382px] rounded border border-secondary">
+      <div className={'flex flex-wrap relative h-full'}>
+        <p
+          className={`background bg-secondary absolute w-1/2 top-0 bottom-0 rounded transition-all duration-300 ease-in-out ${
+            isCurrentEpoch ? 'left-0 right-1/2' : 'right-0 left-1/2'
+          }`}
+        ></p>
+        <p
+          className={`bg-transparent absolute cursor-pointer w-1/2 left-0 top-1/2 -translate-y-1/2 text-center font-medium transition-all duration-300 ease-in-out ${
+            isCurrentEpoch ? 'text-white' : 'text-secondary'
+          }`}
+          onClick={() => setIsCurrentEpoch(true)}
+        >
+          Current Epoch
+        </p>
+        <p
+          className={`bg-transparent absolute cursor-pointer w-1/2 right-0 top-1/2 -translate-y-1/2 text-center font-medium transition-all duration-300 ease-in-out ${
+            isCurrentEpoch ? 'text-secondary' : 'text-white'
+          }`}
+          onClick={() => setIsCurrentEpoch(false)}
+        >
+          Previous Epoch
+        </p>
+        {/*<button*/}
+        {/*  className={`${*/}
+        {/*    activeDay && epochToShowWinners === activeDay - BigInt(1) ? 'btn-primary' : 'btn-primary-inverted'*/}
+        {/*  } btn-large font-medium w-full xl:w-auto`}*/}
+        {/*  onClick={setPreviousEpochToShowWinners}*/}
+        {/*>*/}
+        {/*  Current Epoch*/}
+        {/*</button>*/}
+        {/*<button*/}
+        {/*  className={`${*/}
+        {/*    epochToShowWinners === BigInt(10) ? 'btn-primary' : 'btn-primary-inverted'*/}
+        {/*  } btn-large font-medium w-full xl:w-auto border-2`}*/}
+        {/*  onClick={() => setEpochToShowWinners(BigInt(10))}*/}
+        {/*>*/}
+        {/*  10 Epoch*/}
+        {/*</button>*/}
+      </div>
     </div>
   );
 }
