1fe1ff0d9920322da8d61759232fa701654c3342
Ali Mahdiyar <alimahdiyar77@gmail.com>
2023-08-04 01:39:06 +0330

    refactor: support goerli on production build

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -1,7 +1,6 @@
-import { IS_PRODUCTION } from 'utils/env';
 import { arbitrum, goerli } from 'wagmi/chains';
 
-export const chains = IS_PRODUCTION ? [arbitrum] : [arbitrum, goerli];
+export const chains = [arbitrum, goerli];
 
 export function isSupportedChain(chainId: number | null | undefined) {
   // return !!chainId && !!SupportedChainId[chainId];
