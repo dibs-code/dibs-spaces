6a89d57bbcc71aff0895b087bda3da59850e8dc9
Amirerfan Siamaki <amirerfan.siamaki@gmail.com>
2023-08-14 19:23:30 +0200

    Rewards Page

--- src/components/rewards/PairRewarderRewards.tsx
+++ src/components/rewards/PairRewarderRewards.tsx
@@ -3,8 +3,8 @@ import RewardToken from 'components/RewardToken';
 import { usePairRewarder } from 'hooks/dibs/usePairRewarder';
 import { usePairRewarderRewards, useWonPairRewarders } from 'hooks/dibs/usePairRewarderRewards';
 import React from 'react';
-import { Link } from 'react-router-dom';
-import RoutePath from 'routes';
+// import { Link } from 'react-router-dom';
+// import RoutePath from 'routes';
 import { PairRewarderLeaderBoardRewardItem } from 'types';
 import { Address, useAccount } from 'wagmi';
 
@@ -24,11 +24,7 @@ const PairRewarderClaimButton = ({
   const { write: claim, isLoading: isLoadingClaim } = usePairRewarderWrite(claimRewardConfig);
 
   return (
-    <button
-      disabled={isLoadingClaim}
-      className="btn-primary btn-large font-medium mt-4 w-full xl:w-auto px-8 border-2 mx-2 text-white bg-primary shadow-primary-xl hover:bg-primary-light"
-      onClick={claim}
-    >
+    <button disabled={isLoadingClaim} className="btn btn--secondary-outlined" onClick={claim}>
       Claim
     </button>
   );
@@ -38,39 +34,51 @@ export const PairRewarderRewards = ({ pairRewarderAddress }: { pairRewarderAddre
   const { rewards } = usePairRewarderRewards(pairRewarderAddress);
   const { pairName } = usePairRewarder(pairRewarderAddress);
   if (rewards === null) {
-    return <div>Loading...</div>;
+    return null;
   }
   return (
-    <div className="bg-white shadow-primary-xl rounded-lg">
-      <div className={'p-4'}>
-        {pairName}
-        <Link
-          to={RoutePath.PAIR_REWARDER_LEADERBOARD.replace(':address', pairRewarderAddress)}
-          className={'btn-link btn-large'}
-        >
-          view leaderboard -&gt;
-        </Link>
-      </div>
-      <table className="w-full text-center">
-        <thead className="bg-primary text-white">
+    <>
+      {/*<div className={'p-4'}>*/}
+      {/*  {pairName}*/}
+      {/*  <Link*/}
+      {/*    to={RoutePath.PAIR_REWARDER_LEADERBOARD.replace(':address', pairRewarderAddress)}*/}
+      {/*    className={'btn-link btn-large'}*/}
+      {/*  >*/}
+      {/*    view leaderboard -&gt;*/}
+      {/*  </Link>*/}
+      {/*</div>*/}
+      <table className="w-full text-center border-separate border-spacing-y-3 rounded">
+        <thead className="text-white">
           <tr>
-            <th className="p-4">Day</th>
-            <th className="p-4">Your Rank</th>
-            <th className="p-4">Reward</th>
-            <th className="p-4">Claim</th>
+            <th className="py-2 text-left pl-8">Pair Joined</th>
+            <th className="py-2 text-left">Your Volume</th>
+            <th className="py-2 text-left">Your Position</th>
+            <th className="py-2 text-left">Your Reward</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
           {rewards.map((rewardItem) => (
-            <tr key={rewardItem.day.toString()} className="border-t border-gray">
-              <td className="p-4">{rewardItem.day.toString()}</td>
-              <td className="p-4">{rewardItem.rank}</td>
-              <td className="p-4">
+            <tr key={rewardItem.day.toString()} className="text-white text-left bg-gray2">
+              <td className="pl-8 rounded-l py-5">
+                <span className="flex flex-col justify-center ">
+                  <span className="flex gap-3">
+                    <img src="/assets/images/pair-coin-icon.svg" alt="" />
+                    <span>
+                      <p>{pairName}</p>
+                      <p className="text-secondary text-sm">Volatile</p>
+                    </span>
+                  </span>
+                </span>
+              </td>
+              <td>
                 {rewardItem.rewardTokensAndAmounts.map((obj) => (
                   <RewardToken key={obj.token} rewardTokenAddress={obj.token} rewardTokenAmount={obj.amount} />
                 ))}
               </td>
-              <td className="p-4">
+              <td>{rewardItem.rank}</td>
+              <td>2000 DEUS + 300 BUSD ≈ $32,195</td>
+              <td className="py-4 pr-8 rounded-r w-36">
                 {rewardItem.claimed ? (
                   <button className="btn-primary btn-large font-medium mt-4 w-full xl:w-auto px-5 border-2 mx-2 text-white bg-gray shadow-primary-xl">
                     Claimed
@@ -83,7 +91,7 @@ export const PairRewarderRewards = ({ pairRewarderAddress }: { pairRewarderAddre
           ))}
         </tbody>
       </table>
-    </div>
+    </>
   );
 };
 
--- src/pages/rewards.tsx
+++ src/pages/rewards.tsx
@@ -1,28 +1,77 @@
 // import {faCopy} from "@fortawesome/pro-regular-svg-icons";
 // import { faTicket } from '@fortawesome/pro-solid-svg-icons';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Address } from 'abitype';
 import { WonPairRewardersRewards } from 'components/rewards/PairRewarderRewards';
 import React from 'react';
+import { useParams } from 'react-router-dom';
+
+import { LeaderBoardEpochButtons } from '../components/pairIsolated/LeaderBoardEpochButtons';
+import { usePairRewarderLeaderboard } from '../hooks/dibs/usePairRewarderLeaderboard';
 
 const Rewards = () => {
-  return (
-    <div className={'page-spacing'}>
-      <main className={'main-spacing'}>
-        <>
-          <header className={'border-b pb-4 mb-16'}>
-            <h2>Rewards</h2>
-          </header>
+  const params = useParams();
 
-          <main>
-            <section>
-              <header className="flex flex-row items-center text-black mb-6">
-                {/*<FontAwesomeIcon style={{ fontSize: 24 }} icon={faTicket} />*/}
-                <p className="text-22 ml-2 mt-0.5">Pair isolated leaderboard rewards</p>
-              </header>
-              <WonPairRewardersRewards />
-            </section>
-          </main>
-        </>
+  const pairRewarderAddress =
+    params.address === 'test'
+      ? // '0x6cB66a0762E7Ce3c0Abc9d0241bF4cfFc67fcdA1' // has day 10 winners
+        '0x21DAcb323a7a23E8B70BA96f2D472bbA92A94D9c' // has day 21 subgraph data
+      : (params.address as Address);
+
+  const { selectedEpoch, selectPreviousEpoch, selectCurrentEpoch, activeDay, setSelectedEpoch } =
+    usePairRewarderLeaderboard(pairRewarderAddress);
+
+  return (
+    <div className="page">
+      <main>
+        <section className="px-8 py-7 rounded bg-primary mb-8 flex w-full justify-between">
+          <div className="section--left pr-6 max-w-[840px]">
+            <h1 className="text-[32px] font-bold text-secondary mb-3">Rewards</h1>
+            <p className="text-xl">Monitor your progress and retrieve your earnings here.</p>
+          </div>
+          <div className="section--right items-center justify-end">
+            <img src="/assets/images/header/reward-icon.svg" alt="" className="w-24 h-24 mr-5" />
+          </div>
+        </section>
+
+        <section className="mb-8 flex w-full gap-6">
+          <div className="card-green rounded flex flex-col gap-20 bg-gray2 py-7 px-9 flex-1">
+            <p className="card-title text-white font-medium text-2xl">Pairs Joined</p>
+            <span className="flex gap-4 items-center ml-auto">
+              <p className="font-medium text-[32px] text-white">12</p>
+            </span>
+          </div>
+          <div className="card-red rounded flex flex-col gap-20 bg-gray2 py-7 px-9 flex-1">
+            <p className="card-title text-white font-medium text-2xl">Volume Generated</p>
+            <span className="flex gap-4 items-center ml-auto">
+              <p className="font-medium text-[32px] text-white">$322,730</p>
+            </span>
+          </div>
+          <div className="card-yellow rounded flex flex-col gap-20 bg-gray2 py-7 px-9 flex-1">
+            <p className="card-title text-white font-medium text-2xl">Rewards Earned</p>
+            <span className="flex gap-4 items-center ml-auto">
+              <p className="font-medium text-[32px] text-white">$730</p>
+            </span>
+          </div>
+        </section>
+
+        <section className="actions flex justify-start mb-4 h-[52px]">
+          <LeaderBoardEpochButtons
+            selectedEpoch={selectedEpoch}
+            selectPreviousEpoch={selectPreviousEpoch}
+            selectCurrentEpoch={selectCurrentEpoch}
+            activeDay={activeDay}
+            setSelectedEpoch={setSelectedEpoch}
+          />
+        </section>
+
+        <section className="border border-primary rounded p-8 pt-0 pb-6">
+          {/*<header className="flex flex-row items-center text-black mb-6">*/}
+          {/*  /!*<FontAwesomeIcon style={{ fontSize: 24 }} icon={faTicket} />*!/*/}
+          {/*  /!*<p className="text-22 ml-2 mt-0.5">Pair isolated leaderboard rewards</p>*!/*/}
+          {/*</header>*/}
+          <WonPairRewardersRewards />
+        </section>
       </main>
     </div>
   );
